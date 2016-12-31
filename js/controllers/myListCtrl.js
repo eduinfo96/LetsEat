cedd4fd6d2355d2e9d434ac24e3beb242ac76085
Theo Beck <eduinfo96@yahoo.com>
2016-12-31 04:49:04 -0600

    Added checkbox

--- js/controllers/myListCtrl.js
+++ js/controllers/myListCtrl.js
@@ -1,3 +1,7 @@
 angular.module( 'app' ).controller( 'myListCtrl', function($scope, $rootScope, mainService) {
-  
+  $scope.visited = false;
+
+  $scope.crossMe = function(){
+    $scope.visited = true;
+  }
 });
--- js/directives/myListDir.js
+++ js/directives/myListDir.js
@@ -1,6 +1,6 @@
 angular.module( 'app' ).directive('myListDir', function() {
     return {
-      template:'<li>{{item.name}}, {{item.address}}, {{item.city}} </li>'
+      template:'<div class="rest-info"><li style="list-style-type: circle;" ng-class="{cross: visited}">{{item.name}},  {{item.address}},  {{item.city}}</li></div> <input type="checkbox" ng-click="crossMe()" ng-model="visited"> </input> '
       , restrict: 'E'
       , scope: {
           item: '='
